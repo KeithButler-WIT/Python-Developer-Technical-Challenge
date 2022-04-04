f037d5db316301104415b334be985bf76d283fdf
KeithButler-WIT <20089137@mail.wit.ie>
2022-04-04 20:34:02 +0100

    added endpoint to get car by id

---
+++ functions/get_car_by_id/app.py
@@ -0,0 +1,79 @@
+!/usr/bin/env python3
+
+import os
+import json
+import boto3
+import logging
+from botocore.exceptions import ClientError
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+def get_ddb_connection():
+    ENV = os.environ['Environment']
+    ddbclient=''
+    if ENV == 'local':
+        ddbclient = boto3.client('dynamodb', endpoint_url='http://dynamodb:8000/')
+    else:
+        ddbclient = boto3.client('dynamodb')
+    return ddbclient
+
+def lambda_handler(event, context):
+    chassno = event['queryStringParameters']['chassis_no']
+
+    try:
+        ddbclient = get_ddb_connection()
+        response = ddbclient.get_item(
+            TableName=os.environ['DDBTableName'],
+            Key={
+                'chassis_no':{'N': chassno},
+            },
+            AttributesToGet=[
+                'make',
+                'model',
+                'year',
+                'id',
+                'last_updated',
+                'price',
+            ],
+        )
+
+        logging.info(response)
+
+        if 'Item' in response:
+            low_level_data = dict(response['Item'])
+
+            boto3.resource('dynamodb')
+            deserializer = boto3.dynamodb.types.TypeDeserializer()
+            python_data = {k: deserializer.deserialize(v) for k,v in low_level_data.items()}
+            logging.info(python_data)
+
+            return {
+                'statusCode': response['ResponseMetadata']['HTTPStatusCode'],
+                'headers': {
+                    'Content-Type': 'application/json',
+                    'Access-Control-Allow-Origin':'*'
+                },
+                'body': json.dumps({
+                    'message': python_data,
+                }),
+            }
+
+        else:
+            return {
+                'statusCode': '404',
+                'headers': {
+                    'Content-Type': 'application/json',
+                    'Access-Control-Allow-Origin':'*'
+                },
+                'body': json.dumps({
+                    'message': 'Item not found',
+                }),
+            }
+
+    except ddbclient.exceptions.ResourceNotFoundException as e:
+        logging.error('Cannot do operations on a non-existent table')
+        raise e
+    except ClientError as e:
+        logging.error('Unexpected error')
+        raise e
